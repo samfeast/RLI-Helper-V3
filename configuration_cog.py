8dcb061e7629fcd75be48fe1cab1cca3d688cf82
Sam Feast <sfeast43@gmail.com>
2022-04-13 18:20:13 +0100

    Fixed errors due to changed file structure

--- configuration_cog.py
+++ configuration_cog.py
@@ -18,29 +18,29 @@ class configuration(commands.Cog):
     # Adds a new word to watched_words.json in lowercase. Requires the listener cog to be reloaded to commit changes
     @watchlist_group.command(description="Add a word to the watchlist.")
     async def add(self, interaction: discord.Interaction, word: str):
-        with open("json/watched_words.json", "r") as read_file:
-            watchlist = json.load(read_file)
-        if word in watchlist["list"]:
+        with open("json/config.json", "r") as read_file:
+            config = json.load(read_file)
+        if word in config["watchlist"]:
             await interaction.response.send_message("Word is already on the watchlist.", ephemeral=True)
         else:
-            watchlist["list"].append(word.lower())
+            config["watchlist"].append(word.lower())
 
-            with open("json/watched_words.json", "w") as write_file:
-                json.dump(watchlist, write_file, indent=2)
+            with open("json/config.json", "w") as write_file:
+                json.dump(config, write_file, indent=2)
 
             await interaction.response.send_message("Word added to the watchlist.\nUse >reload listener to commit changes.", ephemeral=True)
 
     # Removes a word from watched_words.json. Requires the listener cog to be reloaded to commit changes
     @watchlist_group.command(description="Remove a word from the watchlist.")
     async def remove(self, interaction: discord.Interaction, word: str):
-        with open("json/watched_words.json", "r") as read_file:
-            watchlist = json.load(read_file)
+        with open("json/config.json", "r") as read_file:
+            config = json.load(read_file)
 
-        if word.lower() in watchlist["list"]:    
-            watchlist["list"].remove(word.lower())
+        if word.lower() in config["watchlist"]:    
+            config["watchlist"].remove(word.lower())
                 
-            with open("json/watched_words.json", "w") as write_file:
-                json.dump(watchlist, write_file, indent=2)
+            with open("json/config.json", "w") as write_file:
+                json.dump(config, write_file, indent=2)
 
             await interaction.response.send_message("Word removed from the watchlist.\nUse >reload listener to commit changes.", ephemeral=True)
         else:
@@ -49,12 +49,12 @@ class configuration(commands.Cog):
     # Prints the watchlist
     @watchlist_group.command(description="Show the watchlist.")
     async def show(self, interaction: discord.Interaction):
-        with open("json/watched_words.json", "r") as read_file:
-            watchlist = json.load(read_file)
+        with open("json/config.json", "r") as read_file:
+            config = json.load(read_file)
 
         # .format is used as f-string formatting does not allow for backslashes
         # .join joins all of the items in the list into a string separated by "\n"
-        await interaction.response.send_message("```Use >reload listener to ensure this is up to date.\n\n{}```".format("\n".join(watchlist["list"])), ephemeral=True)
+        await interaction.response.send_message("```Use >reload listener to ensure this is up to date.\n\n{}```".format("\n".join(config["watchlist"])), ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(configuration(bot))
